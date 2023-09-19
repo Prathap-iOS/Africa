634165b525a1c7f5a6c279a6d37a3f093e93422d
NowFloats <prathap.reddy@nowfloats.com>
2023-09-19 19:05:21 +0530

    Fetch JSON data and show it on UI

---
+++ Africa/Extension/CodableBundleExtension.swift
@@ -0,0 +1,33 @@
+//
+//  CodableBundleExtension.swift
+//  Africa
+//
+//  Created by Prathap Reddy on 19/09/23.
+//
+
+import Foundation
+
+extension Bundle {
+    func decode(_ file: String) -> [CoverImage] {
+        // 1. Locate the JSON file
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle")
+        }
+        
+        // 2. Create a propert for Data
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle")
+        }
+        
+        // 3. Create a decoder
+        let decoder = JSONDecoder()
+        
+        // 4. Create a property for the decoded data
+        guard let loaded = try? decoder.decode([CoverImage].self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle")
+        }
+        
+        // 5. Return the ready-to-use data
+        return loaded
+    }
+}
---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Prathap Reddy on 19/09/23.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
