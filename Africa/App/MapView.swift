29b4581723d091ba3b534508e1eeefae95c4bf1b
NowFloats <prathap.reddy@nowfloats.com>
2023-09-20 23:11:45 +0530

    MapView added in Locations tab

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -6,10 +6,40 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+        
+        return mapRegion
+    }()
+    
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+    
     var body: some View {
-        Text("Map")
+        // MARK: - BASIC MAP
+//        Map(coordinateRegion: $region)
+        
+        // MARK: - ADVANCED MAP
+        Map(coordinateRegion: $region, annotationItems: locations, annotationContent: { item in
+            // PIN: OLD STYLE (Always Static)
+//            MapPin(coordinate: item.location, tint: .accentColor)
+            
+            // MARKER: NEW STYLE (Always Static)
+//            MapMarker(coordinate: item.location, tint: .accentColor)
+            
+            // CUSTOM BASIC ANNOTATION (It could be interactive)
+            MapAnnotation(coordinate: item.location) {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32, alignment: .center)
+            }
+        })
     }
 }
 
